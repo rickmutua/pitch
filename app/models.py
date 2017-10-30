983cf2b8d2e4affa0f8d6f10ff05477329725de2
Erick <rick.mutua12@gmail.com>
2017-10-30 19:01:16 +0300

    update added user class in module to hold user data configured class user

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+
+    return User.query.get(int(user_id))
 
 
 class PitchCategory(db.Model):
@@ -41,7 +50,7 @@ class Review(db.Model):
     pitch_idea = db.Column(db.String)
 
     posted = db.Column(db.DateTime, default=datetime.utcnow)
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def save_review(self):
         db.session.add(self)
@@ -51,6 +60,39 @@ class Review(db.Model):
     def get_reviews(cls, id):
         reviews = Review.query.filter_by(movie_id=id).all()
         return reviews
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(255), index = True)
+
+    email = db.Column(db.String(255), unique=True, index=True)
+
+    password_hash = db.Column(db.String(255))
+
+    category = db.relationship('PitchCategory', backref='user', lazy='dynamic')
+
+    reviews = db.relationship('Review', backref='user', lazy='dynamic')
+
+
+    @property
+    def password(self):
+        raise AttributeError('Access to the password attribute Denied!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
 
 
 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import Pitch, Review
+from app.models import Pitch, Review, User
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -28,7 +28,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db, Pitch = Pitch, Review = Review )
+    return dict(app = app,db = db, Pitch = Pitch, Review = Review, User = User)
 
 
 if __name__ == '__main__':
